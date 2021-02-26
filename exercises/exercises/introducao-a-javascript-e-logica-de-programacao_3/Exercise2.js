d2c9a2409ec3881d65c39066fe56c9b0f99f499f
Gildeir <gildeirlopes@gmail.com>
2021-02-26 19:19:23 -0300

    Adding exercise2

---
+++ exercises/exercises/introducao-a-javascript-e-logica-de-programacao_3/Exercise2.js
@@ -0,0 +1,10 @@
+/* 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+ */
+let size = 4;
+let symbol = '*';
+let inputLine = '';
+
+for (indexColuna = 0; indexColuna < size; indexColuna += 1){
+    inputLine += symbol;
+    console.log(inputLine)
+}
