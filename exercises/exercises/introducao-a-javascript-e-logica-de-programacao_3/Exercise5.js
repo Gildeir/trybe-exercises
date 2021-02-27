dff2f8f657623853edcb51f33c5973d806d0caac
Gildeir <gildeirlopes@gmail.com>
2021-02-26 21:08:46 -0300

    Adiciona assignment 5

--- exercises/exercises/introducao-a-javascript-e-logica-de-programacao_3/Exercise5.js
+++ exercises/exercises/introducao-a-javascript-e-logica-de-programacao_3/Exercise5.js
@@ -1,22 +1,21 @@
-/* Agora inverta o lado do triângulo. Por exemplo:
+/* 5- Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
  */
 
- 
-
-let n = 5;
+let n = 7;
+let middle = (n + 1) / 2;
+let controlLeft = middle;
+let controlRight = middle;
 let symbol = '*';
-let inputLine = '';
-let inputPosition = n;
-
-for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
-  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
-    if (columnIndex < inputPosition) {
-      inputLine = inputLine + ' ';
+for (let line = 1; line <= middle; line += 1) {
+  let outputLine = '';
+  for (let col = 1; col <= n; col += 1) {
+    if (col == controlLeft || col == controlRight || line == middle) {
+      outputLine += symbol;
     } else {
-      inputLine = inputLine + symbol;
+      outputLine += ' ';
     }
   }
-  console.log(inputLine);
-  inputLine = '';
-  inputPosition -= 1;
-};
+  controlLeft -= 1;
+  controlRight += 1;
+  console.log(outputLine);
+}
